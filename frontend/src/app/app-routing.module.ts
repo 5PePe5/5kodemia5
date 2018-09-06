dcf7987e354717518917ffd67379d5b2fffe7abd
PePw <compuaztec75@gmail.com>
2018-09-06 18:03:27 -0500

    se agregan router

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import{UsuariosComponent} from './usuarios/usuarios.component'
+import { RouterModule, Routes } from '@angular/router';
+import{AgregarusuariosComponent} from './agregarusuarios/agregarusuarios.component'
 
+
+const routes: Routes = [
+  {
+    path: 'usuarios',
+    component: UsuariosComponent
+  },
+  {
+path: 'agregarusuarios',
+   component: AgregarusuariosComponent
+  }
+
+]
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  
+    imports: [ RouterModule.forRoot(routes) ],
+    exports: [RouterModule]
+  
 })
 export class AppRoutingModule { }
